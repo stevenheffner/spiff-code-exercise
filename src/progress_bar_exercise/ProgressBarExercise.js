05d0591c27402b43d6a561540b97a17ecbd21cf7
steven heffner <>
2021-07-26 20:49:44 -0600

    added fancy breakpoints prop

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Exercise from "../exercise/Exercise"
 import "./Solution.scss"
 import "../shared_styles/typography.scss"
@@ -7,7 +7,7 @@ const ProgressBarExercise = () => {
   return (
     <div className="progress-bar-exercise">
       <Exercise
-        solution={<Solution />}
+        solution={<Solution breakpoints={[25, 50, 75]} />}
         specsUrl="https://github.com/SpiffInc/spiff_react_exercises/issues/1"
         title="Progress Bar Exercise"
       />
@@ -26,26 +26,79 @@ const useApi = () => {
 
   return {
     state,
+    isIdle: state === "idle",
+    isComplete: state === "complete",
+    isLoading: state === "loading",
     apiCall: () => setState("loading"),
     complete: () => setState("complete"),
     reset: () => setState("idle"),
   }
 }
 
-const Solution = () => {
-  const { apiCall, state, complete, reset } = useApi()
+const Solution = ({ breakpoints: breakPointsProp }) => {
+  const [breakpoints] = useState([0, ...(breakPointsProp || []), 90])
+
+  const [currentBreakpointIndex, setCurrentBreakpointIndex] = useState(0)
+
+  const {
+    apiCall,
+    state,
+    isLoading,
+    isIdle,
+    isComplete,
+    complete,
+    reset,
+  } = useApi()
+
+  const useBreakpoints = Boolean(breakPointsProp)
+
+  const timing = (15 / breakpoints.length) * 1000
+
+  useEffect(() => {
+    if (isLoading && currentBreakpointIndex !== breakpoints.length - 1) {
+      if (currentBreakpointIndex === 0 && !isIdle) {
+        setCurrentBreakpointIndex(1)
+      } else {
+        setTimeout(() => {
+          setCurrentBreakpointIndex((prevIndex) => prevIndex + 1)
+        }, timing)
+      }
+    }
+  }, [timing, currentBreakpointIndex, breakpoints.length, isLoading, isIdle])
+
+  useEffect(() => {
+    if (isIdle) {
+      setCurrentBreakpointIndex(0)
+    }
+  }, [isIdle])
 
   return (
     <div className="container">
-      <div className={`progress-bar progress-bar-${state}`} />
+      <div
+        className={`progress-bar ${
+          useBreakpoints && isComplete
+            ? "progress-bar-complete"
+            : useBreakpoints
+            ? ""
+            : `progress-bar-${state}`
+        }`}
+        style={
+          useBreakpoints && !isComplete
+            ? {
+                transition: `width ease-in-out ${timing}ms`,
+                width: `${breakpoints[currentBreakpointIndex]}%`,
+              }
+            : {}
+        }
+      />
       <div className="buttons-container">
-        {state === "idle" || state === "loading" ? (
+        {isIdle || isLoading ? (
           <button
-            disabled={state !== "idle"}
+            disabled={!isIdle}
             onClick={() => apiCall()}
             className="small-header green button"
           >
-            {state === "loading" ? "loading..." : "start request"}
+            {isLoading ? "loading..." : "start request"}
           </button>
         ) : (
           <button onClick={() => reset()} className="small-header green button">
@@ -53,7 +106,7 @@ const Solution = () => {
           </button>
         )}
         <button
-          disabled={state !== 'loading'}
+          disabled={!isLoading}
           onClick={() => complete()}
           className="small-header button red finish-button"
         >
